496cc75784267f1bcf493bc3af18847c5570956e
nishanthan-k <krn.nishanthan@gmail.com>
2024-05-01 16:34:56 +0530

    add user route link to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,10 @@ const Header = () => {
     <header className='h-20 flex items-center justify-between px-3 md:px-10 bg-red-500'>
       <h1 className='text-3xl font-bold' >Redux Blog</h1>
       <nav>
-        <ul className='flex justify-between flex-wrap w-28'>
+        <ul className='flex gap-4'>
           <li className='text-lg' ><NavLink to="/">Home</NavLink></li>
           <li className='text-lg' ><NavLink to="/post">Post</NavLink></li>
+          <li className='text-lg' ><NavLink to="/user">User</NavLink></li>
         </ul>
       </nav>
     </header>
