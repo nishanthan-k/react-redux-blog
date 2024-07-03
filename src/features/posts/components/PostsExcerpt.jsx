070f1cdbf6d3ff0d80ae008c6ab00c5f8700c385
Nishanthan K <nishanthan.k@amphisoft.in>
2024-07-03 09:49:17 +0530

    fix: removed scroll post card

--- src/features/posts/components/PostsExcerpt.jsx
+++ src/features/posts/components/PostsExcerpt.jsx
@@ -6,7 +6,7 @@ import ReactionButtons from './ReactionButtons';
 
 const PostsExcerpt = ({ post }) => {
   return (
-    <div className='bg-slate-100 border border-slate-500 rounded-xl w-5/6 max-w-3xl max-h-72 min-h-max mt-6 px-3 py-2 overflow-scroll'>
+    <div className='bg-slate-100 border border-slate-500 rounded-xl w-5/6 max-w-3xl max-h-72 min-h-max mt-6 px-3 py-2 overflow-hidden'>
       <section className='h-3/5 overflow-hidden flex flex-col gap-2'>
         <h2 className="text-2xl font-medium">{post.title.substring(0, 40)}</h2>
         <p className="text-lg">{post.body.substring(0, 75)}</p>
